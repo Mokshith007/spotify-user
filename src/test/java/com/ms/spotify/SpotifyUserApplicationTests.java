2746dd66701754cde2aed11ece8c20cfee1014b6
Ajay Kumar Sharma <ajay14sharma@gmail.com>
2021-04-29 14:54:30 +0530

    AB#1 First Commit

---
+++ src/test/java/com/ms/spotify/SpotifyUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.spotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
